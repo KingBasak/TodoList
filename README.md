52106282e4b08ba208fd541f85c7719688862bef
Kingsuk Basak <69724286+KingBasak@users.noreply.github.com>
2020-09-29 22:45:40 +0530

    V2.1

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+Hello,
+I am Kingsuk Basak and this is my first Project on javascript(Web Development).I made this project to make to-do lists projects on internet a little more viable. Here you can make different lists for different work and organize you to-do list. I will be continuously updating this project to it's best as i learn more.
+You can also find this project on my Portfolio Website.
+
+Who uses to-do list with only one list?
+
 # TodoList
+
+version 1.0
+---------------->
+A simple todo list you can add,strik off and remove any task you want.
+---------------->
+version 2.1
+---------------->
+Now you can add more list to simplify and organize your own TO-DO list.
+Add a different list for your work and a recipe for your favorite food.
+Basic prototype layout.
+---------------->
+version 2.2
+---------------->
+COMING SOON.....
--- script.js
+++ script.js
@@ -1,48 +1,94 @@
-var button = document.getElementById("add");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var listitem = document.querySelector("li");
-var btn = document.getElementsByClassName("DoneBtn");
+var ListBtn = document.getElementById("AddListBtn");
+var ListName = document.getElementById("ListName");
+var DivContainer = document.querySelector(".ListContainer");
 
-function createnewlist() {
-  var Newli = document.createElement("li");
-  var Newbtn = document.createElement("button");
-  var Delbtn = document.createElement("button");
-  Delbtn.appendChild(document.createTextNode("❎"));
-  Newli.appendChild(Delbtn);
-  Newbtn.appendChild(document.createTextNode("✅"));
-  Newli.appendChild(Newbtn);
-  Newli.appendChild(document.createTextNode(" " + input.value));
-  ul.appendChild(Newli);
-  Delbtn.className = "DelBtn";
-  Newbtn.className = "DoneBtn";
-  Newbtn.onclick = TaskDone;
-  Delbtn.onclick = DeleteTask;
-  input.value = "";
-}
-function TaskAfterClick() {
-  if (input.value.length > 0) {
-    createnewlist();
+function CreateNewList() {
+  //Declaring the new HTML tags for new List creation.
+  var NewCard = document.createElement("div");
+  var NewHead = document.createElement("div");
+  var ListHeading = document.createElement("h3");
+  var NewCrossBtn = document.createElement("button");
+  var NewBody = document.createElement("div");
+  var NewTaskInput = document.createElement("input");
+  var NewTaskBtn = document.createElement("button");
+  var NewUl = document.createElement("ul");
+  //Appending all HTML tage properly in order for creation of new List.
+  ListHeading.appendChild(document.createTextNode(ListName.value));
+  NewHead.appendChild(ListHeading);
+  NewCrossBtn.appendChild(document.createTextNode("X"));
+  NewHead.appendChild(NewCrossBtn);
+  NewCard.appendChild(NewHead);
+  NewBody.appendChild(NewTaskInput);
+  NewTaskBtn.appendChild(document.createTextNode("Add task"));
+  NewBody.appendChild(NewTaskBtn);
+  NewBody.appendChild(NewUl);
+  NewCard.appendChild(NewBody);
+  DivContainer.appendChild(NewCard);
+  //Assinging the nesesarry class to the new HTML tags.
+  NewCard.className = "ListCard";
+  NewHead.className = "ListHead";
+  NewCrossBtn.className = "CrossListBtn";
+  NewBody.className = "ListBody";
+  NewTaskInput.className = "TaskInput";
+  NewTaskInput.type = "text";
+  NewTaskInput.placeholder = "Enter Task";
+  NewTaskBtn.className = "AddTaskBtn";
+  NewCrossBtn.onclick = DeleteList;
+  // Code for activating the buttons to add new task.
+  NewTaskBtn.addEventListener("click", AddTaskAfterClick);
+  NewTaskInput.addEventListener("keypress", AddTaskAfterKeypress);
+  function AddTaskAfterClick() {
+    if (NewTaskInput.value.length > 0) {
+      CreateNewTask();
+    }
+  }
+  function AddTaskAfterKeypress(event) {
+    if (NewTaskInput.value.length > 0 && event.keyCode === 13) {
+      CreateNewTask();
+    }
+  }
+  //Code for creating new task.
+  function CreateNewTask() {
+    var Newli = document.createElement("li");
+    var Newbtn = document.createElement("button");
+    var Delbtn = document.createElement("button");
+    Delbtn.appendChild(document.createTextNode("✖"));
+    Newli.appendChild(Delbtn);
+    Newbtn.appendChild(document.createTextNode("✔"));
+    Newli.appendChild(Newbtn);
+    Newli.appendChild(document.createTextNode(NewTaskInput.value));
+    NewUl.appendChild(Newli);
+    Delbtn.className = "TaskDeleteBtn";
+    Newbtn.className = "TaskDoneBtn";
+    Newbtn.onclick = TaskDone;
+    Delbtn.onclick = DeleteTask;
+    NewTaskInput.value = "";
   }
 }
 
-function TaskAfterKeypress(event) {
-  if (input.value.length > 0 && event.keyCode === 13) {
-    createnewlist();
-  }
+//Code for Deleting any task or any list.
+function DeleteList(event) {
+  event.target.parentNode.parentNode.remove();
 }
-
-function TaskAfterDone() {
-  btn[0].parentElement.className = "complete";
-}
-
 function TaskDone(event) {
-  event.target.parentNode.classList.toggle("complete");
+  event.target.parentNode.classList.toggle("TaskComplete");
 }
-
 function DeleteTask(event) {
   event.target.parentNode.remove();
 }
 
-button.addEventListener("click", TaskAfterClick);
-input.addEventListener("keypress", TaskAfterKeypress);
+// Code for activating the buttons to add new List.
+ListBtn.addEventListener("click", AddListAfterClick);
+ListName.addEventListener("keypress", AddTaskAfterKeypress);
+function AddListAfterClick() {
+  if (ListName.value.length > 0) {
+    CreateNewList();
+    ListName.value = "";
+  }
+}
+function AddTaskAfterKeypress(event) {
+  if (ListName.value.length > 0 && event.keyCode === 13) {
+    CreateNewList();
+    ListName.value = "";
+  }
+}
